960059ea9748370f19be8ec40e46f1689d8066a6
KillaMeep <killerpenguin729@gmail.com>
2024-10-03 21:41:15 -0600

    refactored navbar

--- limiter.js
+++ limiter.js
@@ -7,9 +7,8 @@ function loadParticlesConfig() {
             const screenWidth = window.innerWidth;
 
             // Example logic: Adjust particle count based on screen size
-            const particleCount = Math.max(200, Math.min(Math.floor(screenWidth / 5), 800)); // Adjust as needed
+            const particleCount = Math.max(200, Math.min(Math.floor(screenWidth / 5), 700)); // Adjust as needed
             config.particles.number.value = particleCount;  // Update the number of particles
-
             // Initialize particles.js with the modified config
             particlesJS('particles-js', config);
         })
